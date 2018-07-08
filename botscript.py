227148c95c4c74b044f96d74eb2c1bd471dd1749
David <dave847@gmail.com>
2018-07-08 00:29:16 -0400

    Initial Github Commit

--- botscript.py
+++ botscript.py
@@ -2,10 +2,11 @@ import tweepy
 import random
 import os
 
+print(os.environ)
 CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
-OAUTH_TOKEN = os.environ['TWITTER_OAUTH_TOKEN']
-OAUTH_TOKEN_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
+ACCESS_KEY = os.environ['TWITTER_OAUTH_TOKEN']
+ACCESS_SECRET = os.environ['TWITTER_OAUTH_TOKEN_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
@@ -17,6 +18,8 @@ try:
 except tweepy.TweepError as error:
     if error.api_code == 187:
         # Do something special
+        msg  = "this bot is stupid and tried to tweet the same thing too quickly" + str(random.randint(1,100001))
+        api.update_status(msg)
         print('duplicate message')
     else:
        raise error
